9c08b2701dd4824805895aad3f21947035a8250f
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-06-04 21:55:50 -0400

    Set up NuxtUI

--- app.vue
+++ app.vue
@@ -1,13 +1,20 @@
 <script setup>
 // Everything you wrap around NuxtPage or place beside Nuxtpage will apply to each page you make
 // This allows you to add global content
+
+// Look this up: https://ui.nuxt.com/components/input#within-a-formfield
 </script>
 
 <template>
-    <div>
-        <p>This message always appears atop each pages</p>
-        <NuxtPage />
-    </div>
+    <UApp>
+        <div>
+            <p>This message always appears atop each pages</p>
+            <NuxtPage />
+        </div>
+    </UApp>
 </template>
 
-<style></style>
+<style>
+@import "tailwindcss";
+@import "@nuxt/ui";
+</style>
