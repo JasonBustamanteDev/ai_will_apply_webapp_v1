afcfd4dca97747ace0347bd2274c1867f3a34ab8
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-06-08 14:54:12 -0400

    Add new private .env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,18 @@
 export default defineNuxtConfig({
     compatibilityDate: "2025-05-15",
     devtools: { enabled: true },
+    devServer: {
+        port: 4010,
+    },
 
     modules: [
-      "@nuxt/eslint",
-      "@nuxt/fonts",
-      "@nuxt/icon",
-      "@nuxt/image",
-      "@nuxt/test-utils",
-      "@nuxt/ui",
-      "@pinia/nuxt",
+        "@nuxt/eslint",
+        "@nuxt/fonts",
+        "@nuxt/icon",
+        "@nuxt/image",
+        "@nuxt/test-utils",
+        "@nuxt/ui",
+        "@pinia/nuxt",
     ],
 
     runtimeConfig: {
@@ -17,6 +20,8 @@ export default defineNuxtConfig({
         SUPABASE_DB_PASSWORD: process.env.SUPABASE_DB_PASSWORD,
         SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
         SUPABASE_SERVICE_ROLE_SECRET: process.env.SUPABASE_SERVICE_ROLE_SECRET,
+        GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+        GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
 
         public: {
             // Public keys (available on both client and server)
