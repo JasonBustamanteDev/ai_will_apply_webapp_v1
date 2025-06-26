fd037102544c9ea921bacfe972a7a73ac0c17e4a
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-06-26 17:46:57 -0400

    use mw

--- server/api/devonly/sample.post.js
+++ server/api/devonly/sample.post.js
@@ -1,7 +1,10 @@
 import { authMiddleware } from "~/server/utils/authMiddleware";
 
 export default defineEventHandler(async (event) => {
-    authMiddleware(event);
-
-    return { detail: "POST worked", result: 'decoded' };
+    try {
+        authMiddleware(event);
+        return { detail: "POST worked", result: "decoded" };
+    } catch (err) {
+        //! Handle error. Make sure to send a similar response object compared to success, with ane rror code 
+    }
 });
