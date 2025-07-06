81ce6360a8256e3fd545d63c69f4c244a9349d7d
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-07-06 19:46:31 -0400

    reorder components

--- ui/profiles/languages/languagesForm.vue
+++ ui/profiles/languages/languagesForm.vue
@@ -22,49 +22,65 @@ const isLastLanguageComplete = computed(() => {
 });
 
 const addLanguage = () => {
-    if (isLastLanguageComplete.value) {
-        languages.value.push({ language: "", proficiency: "" });
-    }
+    languages.value.push({ language: "", proficiency: "" });
 };
 
 const removeLanguage = (index) => {
     languages.value.splice(index, 1);
 };
+
+const onSubmit = () => {
+
+};
 </script>
 
 <template>
-    <AddLanguageButton
-        v-if="languages.length === 0"
-        :isDisabled="!isLastLanguageComplete"
-        @addLangToList="addLanguage"
-    />
-    <div
-        v-for="(lang, index) in languages"
-        :key="index"
-        class="flex items-end gap-3"
-    >
-        <UInput
-            v-model="lang.language"
-            placeholder="Full language name. Ex. 'English'"
-            class="w-64"
-        />
-        <USelect
-            v-model="lang.proficiency"
-            :items="proficiencyOptions"
-            class="w-36"
-        />
-        <UButton
-            icon="i-heroicons-trash"
-            color="red"
-            variant="ghost"
-            @click="removeLanguage(index)"
-            class="mb-1"
-        />
-        <AddLanguageButton
-            v-if="index === languages.length - 1"
-            :isDisabled="!isLastLanguageComplete"
-            @addLangToList="addLanguage"
-        />
+    <div class="flex flex-col gap-3">
+        <p>Enter how many languages you can speak, and at what level</p>
+        <div
+            v-for="(lang, index) in languages"
+            :key="index"
+            class="flex items-end gap-3"
+        >
+            <UInput
+                v-model="lang.language"
+                placeholder="Full language name. Ex. 'English'"
+                class="w-64"
+                highlight 
+                color=""
+            />
+            <USelect
+                v-model="lang.proficiency"
+                :items="proficiencyOptions"
+                class="w-36"
+                highlight 
+                color=""
+            />
+            <UButton
+                icon="i-heroicons-trash"
+                color="red"
+                variant="ghost"
+                @click="removeLanguage(index)"
+                :disabled="languages.length === 1"
+                class="mb-1"
+            />
+            <AddLanguageButton
+                v-if="index === languages.length - 1 || languages.length === 1"
+                :isDisabled="!isLastLanguageComplete"
+                @addLangToList="addLanguage"
+            />
+        </div>
+        <div class="uform-submit-button-container">
+            <UButton
+                type="submit"
+                class="w-full justify-center"
+                @click="onSubmit"
+                color="secondary"
+                >Submit</UButton
+            >
+        </div>
     </div>
     <p>{{ languages }}</p>
 </template>
+
+<style lang="scss"></style>
