aa44bec173727a52195a8e839849526eefd8964b
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-07-10 21:09:16 -0400

    noticing bug with row gen

--- ui/profiles/workExperience/workExperienceForm.vue
+++ ui/profiles/workExperience/workExperienceForm.vue
@@ -3,6 +3,7 @@ import { object, string, boolean } from "yup";
 import { verifyMinStringLength, isValidYearMonth } from "~/shared/helper_methods"; // prettier-ignore
 import { booleanOptions, radioStyleObject } from "~/ui/profiles/shared/util.js";
 import InputLabelSlot from "~/ui/profiles/shared/inputLabelSlot.vue";
+import AddRowButton from "@/ui/profiles/shared/addRowButton.vue";
 
 /*
 job title
@@ -35,19 +36,39 @@ const workExperienceList = ref([
 
         jobTitleError: false,
         companyError: false,
+        yearsError: false,
         currentlyThereError: false,
     },
 ]);
 
+const areAllRowsValid = () => {
+    // Check if each field is filled - render error if it isn't and clear errors if it is
+    for (const obj of workExperienceList.value) {
+        obj.jobTitleError = !verifyMinStringLength(obj.jobTitle, 1);
+        obj.companyError = !verifyMinStringLength(obj.company, 1);
+        obj.yearsError = !obj.yearsError;
+    }
+
+    const errorExists = workExperienceList.value.some(
+        (item) => item.jobTitleError || item.companyError || item.yearsError
+    );
+
+    return !errorExists;
+};
+
 const addExperienceRow = () => {
+    const rowsAreValid = areAllRowsValid();
+    if (!rowsAreValid) return;
+
     workExperienceList.value.push({
         jobTitle: "",
         company: "",
-        years: 1,
+        years: undefined,
         currentlyThere: false,
 
         jobTitleError: false,
         companyError: false,
+        yearsError: false,
         currentlyThereError: false,
     });
 };
@@ -57,75 +78,64 @@ const removeExperienceRow = (index) => {
 };
 
 const onSubmit = () => {
-    // Check if each field is filled - render error if it isn't and clear errors if it is
-    for (const obj of workExperienceList.value) {
-        obj.jobTitleError = !verifyMinStringLength(obj.jobTitle, 1);
-        obj.companyError = !verifyMinStringLength(obj.company, 1);
-    }
-
-    if (
-        workExperienceList.value.some(
-            (item) => item.jobTitleError || item.companyError
-        )
-    ) {
-        return;
-    }
+    const rowsAreValid = areAllRowsValid();
+    if (!rowsAreValid) return;
     console.log("Submit form", workExperienceList.value);
 };
 </script>
 
 <template>
-    <!-- <section>
+    <section class="multiple-forms-container">
+        <div
+            v-for="(row, index) in workExperienceList"
+            :key="index"
+            class="uform-element"
+        >
+            <UInput
+                v-model="row.jobTitle"
+                placeholder=""
+                :highlight="row.jobTitleError"
+                :color="row.jobTitleError ? 'error' : ''"
+            >
+                <InputLabelSlot labelText="Job Title" />
+            </UInput>
+            <UInput
+                v-model="row.company"
+                placeholder=""
+                :highlight="row.companyError"
+                :color="row.companyError ? 'error' : ''"
+            >
+                <InputLabelSlot labelText="Company" />
+            </UInput>
+            <UInputNumber
+                v-model="row.years"
+                :min="1"
+                :max="120"
+                class="w-full"
+                placeholder="Years"
+                :highlight="row.yearsError"
+                :color="row.yearsError ? 'error' : 'neutral'"
+            />
 
-    </section> -->
-    <div
-        v-for="(row, index) in workExperienceList"
-        :key="index"
-        class="uform-element"
+            <URadioGroup
+                v-model="row.currentlyThere"
+                orientation="horizontal"
+                variant="list"
+                :items="currentlyThereOptions"
+                size="lg"
+                :ui="radioStyleObject"
+                class="my-auto"
+            />
+        </div>
+        <p>{{ workExperienceList }}</p>
+    </section>
+
+    <AddRowButton :isDisabled="false" @addRow="addExperienceRow"
+        >Add Experience</AddRowButton
     >
-        <UInput
-            v-model="row.jobTitle"
-            placeholder=""
-            :highlight="row.jobTitleError"
-            :color="row.jobTitleError ? 'error' : ''"
-        >
-            <InputLabelSlot labelText="Job Title" />
-        </UInput>
-        <UInput
-            v-model="row.company"
-            placeholder=""
-            :highlight="row.companyError"
-            :color="row.companyError ? 'error' : ''"
-        >
-            <InputLabelSlot labelText="Company" />
-        </UInput>
-        <UInputNumber
-            v-model="row.years"
-            :min="0"
-            :max="120"
-            class="w-full"
-            color="neutral"
-            placeholder="Years"
-        />
-        <!-- <UInput
-            v-model="row.startDate"
-            placeholder="YYYY-MM"
-            :highlight="row.startDateError"
-            :color="row.startDateError ? 'error' : ''"
-        >
-            <InputLabelSlot labelText="Years" />
-        </UInput> -->
-
-        <URadioGroup
-            v-model="row.currentlyThere"
-            orientation="horizontal"
-            variant="list"
-            :items="currentlyThereOptions"
-            size="lg"
-            :ui="radioStyleObject"
-            class="my-auto"
-        />
-    </div>
+    <AddRowButton :isDisabled="false" @addRow="removeExperienceRow"
+        >Remove Experience</AddRowButton
+    >
 
     <div class="uform-submit-button-container">
         <UButton
