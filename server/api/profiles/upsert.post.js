e48df63c617e8e45281f121670d8dcfdc855498d
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-07-12 13:43:47 -0400

    fix error handlin

--- server/api/profiles/upsert.post.js
+++ server/api/profiles/upsert.post.js
@@ -7,33 +7,34 @@ export default defineEventHandler(async (event) => {
         const body = await readBody(event);
         const { accessToken } = checkIfUserIsAuthenticated(event);
         const supabaseClient = getSupabaseClient(event, accessToken);
-        const { auth_id } = await getSupabaseUserDetails(supabaseClient, accessToken)
+        const { auth_id } = await getSupabaseUserDetails(
+            supabaseClient,
+            accessToken
+        );
 
         const uploadObject = {
             id: auth_id,
             updated_at: getCurrentUTCTimestamp(),
-            
-        }
-        
+        };
 
         const { data, error } = await supabaseClient
             .from("jobSearchProfiles")
-            .upsert({ id: 1, name: 'piano' })
-            .select()
+            .upsert(uploadObject)
+            .select();
 
-        // if (error) {
-        //     setResponseStatus(event, 500);
-        //     return {
-        //         detail: "Error occurred when fetching profile data",
-        //         data: null,
-        //     };
-        // }
+        if (error) {
+            setResponseStatus(event, 500);
+            return {
+                detail: "Error occurred when upserting profile data",
+                data: null,
+            };
+        }
 
-        return { detail: "success", uploadObject };
+        return { detail: "success" };
     } catch (err) {
         const error_code = err?.statusCode || 500;
         const error_message = err?.statusMessage || err?.message || "Something went wrong"; // prettier-ignore
         setResponseStatus(event, error_code);
-        return { detail: error_message, data: null };
+        return { detail: error_message };
     }
 });
