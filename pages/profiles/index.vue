aeee3f3d8216d79c130cf63dda6578664633e0b7
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-07-13 12:41:41 -0400

    Rearrange profile card content

--- pages/profiles/index.vue
+++ pages/profiles/index.vue
@@ -8,28 +8,37 @@ const deleteProfile = () => {};
 </script>
 
 <template>
+    <!-- DaisyUI components used for the cards -->
     <SharedPageContainerWithNavbar>
-        <!-- DaisyUI components used for the cards -->
-        <div
-            @click="createNewProfile"
-            class="cursor-pointer card bg-base-100 w-60 shadow-sm"
-        >
-            <div class="card-body items-center text-center">
-                <div class="w-[40px] mx-auto mb-6">
-                    <AddProfileSvg />
+        <section class="card-container">
+            <div
+                @click="createNewProfile"
+                class="cursor-pointer card bg-base-100 w-60 shadow-sm"
+            >
+                <div class="card-body items-center text-center">
+                    <div class="w-[40px] mx-auto mb-3">
+                        <AddProfileSvg />
+                    </div>
+                    <h2 class="card-title">Create New Profile</h2>
+                    <p>Upload resume then enter your information</p>
                 </div>
-                <h2 class="card-title">Create New Profile</h2>
-                <p>Upload resume then enter your information</p>
             </div>
-        </div>
 
-        <ProfileCard
-            profileName="Canadian Resume"
-            lastModifiedDate="2024-09-08"
-            @editCallback="editProfile"
-            @deleteCallback="deleteProfile"
-        />
+            <ProfileCard
+                profileName="Canadian Resume"
+                lastModifiedDate="2024-09-08"
+                @editCallback="editProfile"
+                @deleteCallback="deleteProfile"
+            />
+        </section>
     </SharedPageContainerWithNavbar>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.card-container {
+    display: grid;
+    grid-template-columns: 0.8fr 1fr 1fr 1fr;
+    grid-template-rows: 200px;
+    gap: 1rem;
+}
+</style>
--- ui/profiles/shared/profileCard.vue
+++ ui/profiles/shared/profileCard.vue
@@ -6,23 +6,36 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="cursor-pointer card bg-base-100 w-60 shadow-sm">
-        <div class="card-body items-center text-center">
+    <div class="card bg-base-100 w-60 shadow-sm">
+        <div class="card-body card-content-custom">
             <h2 class="card-title">{{ props.profileName }}</h2>
-            <p>Last Modified: {{ props.lastModifiedDate }}</p>
-            <div>
-                <UButton color="neutral" @click="$emit('editCallback')"
+            <section>
+                <p>Forms completed: 3/8</p>
+                <p>Last Modified: {{ props.lastModifiedDate }}</p>
+            </section>
+            <section class="flex gap-4">
+                <UButton
+                    color="neutral"
+                    @click="$emit('editCallback')"
+                    class="cursor-pointer"
                     >Edit</UButton
                 >
                 <UButton
                     color="neutral"
                     variant="outline"
                     @click="$emit('deleteCallback')"
+                    class="cursor-pointer"
                     >Delete</UButton
                 >
-            </div>
+            </section>
         </div>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.card-content-custom {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+</style>
