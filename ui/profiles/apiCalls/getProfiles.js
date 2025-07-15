351c5d9aa7067a1fe323668e6488eb02e6bc62c2
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-07-14 23:47:50 -0400

    validate most other schemas

--- ui/profiles/apiCalls/getProfiles.js
+++ ui/profiles/apiCalls/getProfiles.js
@@ -1,8 +1,7 @@
 import { getAuthSessionFromLocalStorage, extractFormattedDate } from "~/shared/client_helpers"; // prettier-ignore
-import { personalDetailsSchema } from "../views/formValidation"; // prettier-ignore
+import { personalDetailsSchema, locationSchema, preferenceSchema, educationSchema, socialSchema } from "../views/formValidation"; // prettier-ignore
 
 const formatData = async (profileList) => {
-
     const rowPromises = profileList.map(async (x) => {
         const createdAt = x.createdAt;
         const updatedAt = x.updatedAt;
@@ -11,23 +10,39 @@ const formatData = async (profileList) => {
             .validate(x.personalDetails)
             .then(() => true)
             .catch(() => false);
-
+        const locationValidation = await locationSchema
+            .validate(x.location)
+            .then(() => true)
+            .catch(() => false);
+        const preferenceValidation = await preferenceSchema
+            .validate(x.preferences)
+            .then(() => true)
+            .catch(() => false);
+        const educationValidation = await educationSchema
+            .validate(x.education)
+            .then(() => true)
+            .catch(() => false);
+        const mediaValidation = await socialSchema
+            .validate(x.mediaLinks)
+            .then(() => true)
+            .catch(() => false);
 
         return {
             profileName: x.profileName,
             lastUpdated: extractFormattedDate(updatedAt || createdAt),
+
+            // prettier-ignore
             forms: {
-                personalDetails: {
-                    data: x.personalDetails,
-                    isComplete: personalDetailsValidation,
-                },
-                location: { data: x.location },
-                preferences: { data: x.preferences },
+                personalDetails: { data: x.personalDetails, isComplete: personalDetailsValidation },
+                location: { data: x.location, isComplete: locationValidation },
+                preferences: { data: x.preferences, isComplete: preferenceValidation },
+
                 languages: { data: x.languages },
                 skills: { data: x.skills },
                 workExperience: { data: x.workExperience },
-                education: { data: x.education },
-                mediaLinks: { data: x.mediaLinks },
+                
+                education: { data: x.education, isComplete: educationValidation },
+                mediaLinks: { data: x.mediaLinks, isComplete: mediaValidation },
             },
         };
     });
