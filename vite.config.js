75581a5dd59430da1aeb71e73708d26ea7b022b3
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-08-09 20:55:33 -0400

    Package install and config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import vue from "@vitejs/plugin-vue";
+import path from 'path'
+
+export default {
+    plugins: [vue()],
+    test: {
+        alias: {
+            "~": path.resolve(__dirname, "./"),
+            "*": path.resolve(__dirname, "./"),
+        },
+    },
+};
