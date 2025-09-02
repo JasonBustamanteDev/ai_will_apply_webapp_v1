d7356efc6ff7fbb474aef995b309bde854f2ddef
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-09-02 17:07:32 -0400

    restructure skeleton

--- test/nuxt/personalDetails.test.ts
+++ test/nuxt/personalDetails.test.ts
@@ -1,7 +1,8 @@
 import { expect, describe, it, beforeEach, assert } from "vitest"; // prettier-ignore
 import { render } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
-// import PersonalDetailsForm from "../../../ui/profiles/views/personalDetails/personalDetailsForm.vue";
+import { mount } from "@vue/test-utils";
+import PersonalDetailsForm from "~/ui/profiles/views/personalDetails/personalDetailsForm.vue";
 
 // let FormComponent = null;
 
@@ -9,8 +10,10 @@ import userEvent from "@testing-library/user-event";
 //     FormComponent = render(PersonalDetailsForm);
 // });
 
-describe("Mandatory Fields", () => {
+describe("Fresh form with nothing filled in", () => {
     it("Should have Age && Years of Experience prefilled", () => {
+        console.log(333, EMPTY_FORM_DATA);
+        // const wrapper = mount(PersonalDetailsForm)
         // Press submit button on form
         // These two fields should have prefilled values
         // If they don't we need to add them to the test that renders red text when mandator
@@ -18,20 +21,25 @@ describe("Mandatory Fields", () => {
     });
 });
 
-describe("Submit examples", () => {
-    it("Should render red error text when mandatory fields that are not prefilled are submitted blank", () => {
-        // Press submit button on form
-        // The First Name, Last Name, Email, Phone Number, and Highest Education Level
-        // above fields should all have red text saying this field is required
-        expect(1).toBe(1);
-    });
+describe("Filled out form", () => {
+    describe("Submit Examples", () => {
+        it("Should render red error text when mandatory fields that are not prefilled are submitted blank", () => {
+            // Press submit button on form
+            // The First Name, Last Name, Email, Phone Number, and Highest Education Level
+            // above fields should all have red text saying this field is required
+            expect(1).toBe(1);
+        });
 
-    it("Should have a successful submit when you fill in the mandatory fields correctly", () => {
-        // First Name, Last Name, Email, Phone Number, and Highest Education Level
-        expect(1).toBe(1);
-    });
+        it("Should have a successful submit when you fill in the mandatory fields correctly", () => {
+            // First Name, Last Name, Email, Phone Number, and Highest Education Level
+            expect(1).toBe(1);
+        });
 
-    it("Should have a successful submit when you fill in all fields correctly", () => {
-        expect(1).toBe(1);
+        it("Should have a successful submit when you fill in all fields correctly", () => {
+            expect(1).toBe(1);
+        });
     });
 });
+
+const EMPTY_FORM_DATA = {};
+const FILLED_FORM_DATA = {};
