79903cab9141ca65cd157ef51d32e8aac6a87d16
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-09-03 13:05:58 -0400

    begin refactoring test methods

--- test/nuxt/personalDetails.test.ts
+++ test/nuxt/personalDetails.test.ts
@@ -1,7 +1,7 @@
 import { expect, describe, it, beforeEach, assert, afterEach } from "vitest"; // prettier-ignore
 import { render, fireEvent } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
-import { mount, VueWrapper } from "@vue/test-utils";
+import { DOMWrapper, mount, VueWrapper } from "@vue/test-utils";
 import PersonalDetailsForm from "~/ui/profiles/views/personalDetails/personalDetailsForm.vue";
 import { PROFILE_FORMS } from "~/shared/utils/globals";
 import { forceLog, delay } from "../util";
@@ -41,6 +41,23 @@ const COMPLETED_FORM_PROPS = {
     },
 };
 
+// prettier-ignore
+const findFormElements = (formWrapper: VueWrapper<any>) => {
+   return {
+       firstName: formWrapper.find(`[data-test="first_name_field"]`),
+       lastName: formWrapper.find(`[data-test="last_name_field"]`),
+       email: formWrapper.find(`[data-test="email_field"]`),
+       phoneNumber: formWrapper.find(`[data-test="phone_number_field"]`),
+       yearsExp: formWrapper.find(`[data-test="years_experience_field"]`),
+       age: formWrapper.find(`[data-test="age_field"]`),
+       highestEducation: formWrapper.find(`[data-test="highest_education_field"]`),
+       gender: formWrapper.find(`[data-test="gender_field"]`),
+       ethnicity: formWrapper.find(`[data-test="ethnicity_field"]`),
+       securityClearance: formWrapper.find(`[data-test="clearance_field"]`),
+       disability: formWrapper.find(`[data-test="disability_field"]`),
+   }
+};
+
 describe("Fresh form with nothing filled in", () => {
     // Render a new instance of an unfilled instance of the form for each test in this describe
     let formWrapper: VueWrapper<any>;
@@ -52,10 +69,9 @@ describe("Fresh form with nothing filled in", () => {
 
     it("Should have Age && Years of Experience prefilled", () => {
         // Find the components that should have default values if form is fresh
-        const age_ticker = formWrapper.find(`[data-test="age_field"]`);
-        const years_experience_input = formWrapper.find(
-            '[data-test="years_experience_field"]'
-        );
+        const formElements = findFormElements(formWrapper);
+        const age_ticker = formElements.age;
+        const years_experience_input = formElements.yearsExp;
 
         // If you want to log the components you found
         // forceLog(age_select_component.html())
@@ -72,16 +88,16 @@ describe("Fresh form with nothing filled in", () => {
         await formWrapper.vm.$nextTick();
 
         // These fields should all have a ring-error attribute
-        const data_attributes = [
-            "first_name_field",
-            "last_name_field",
-            "email_field",
-            "phone_number_field",
-            "highest_education_field",
+        const formElements = findFormElements(formWrapper);
+        const elements = [
+            formElements.firstName,
+            formElements.lastName,
+            formElements.email,
+            formElements.phoneNumber,
+            formElements.highestEducation,
         ];
-        data_attributes.forEach((attr) => {
-            const form_field = formWrapper.find(`[data-test="${attr}"]`);
-            expect(form_field.classes()).toContain("ring-error");
+        elements.forEach((el) => {
+            expect(el.classes()).toContain("ring-error");
         });
     });
 });
@@ -97,56 +113,72 @@ describe("Completed form", () => {
 
     // prettier-ignore
     it("Should have all form fields filled in", () => {
-        // Input fields
-        const first_name_input = formWrapper.find(`[data-test="first_name_field"]`);
-        expect(first_name_input.element.value).toBe("Gustavo")
-
-        const last_name_input = formWrapper.find(`[data-test="last_name_field"]`);
-        expect(last_name_input.element.value).toBe("Markov")
-
-        const email_input = formWrapper.find(`[data-test="email_field"]`);
-        expect(email_input.element.value).toBe('jmarkov@protonmail.com')
-
-        const phone_number_input = formWrapper.find(`[data-test="phone_number_field"]`);
-        expect(phone_number_input.element.value).toBe('6478880000')
-
-        const years_exp_input = formWrapper.find(`[data-test="years_experience_field"]`);
-        expect(years_exp_input.element.value).toBe('10')
-
-        // Number tickers
-        const age_ticker = formWrapper.find(`[data-test="age_field"]`);
-        expect(age_ticker.element.value).toBe("51")
-
-        // Select dropdowns
-        const highest_education_select = formWrapper.find(`[data-test="highest_education_field"]`);
-        expect(highest_education_select.text()).toContain("Bachelor's Degree")
-
-        const gender_select = formWrapper.find(`[data-test="gender_field"]`);
-        expect(gender_select.text()).toContain("Male")
-
-        const ethnicity_select = formWrapper.find(`[data-test="ethnicity_field"]`);
-        expect(ethnicity_select.text()).toContain("White")
-
-        const security_clearance_select = formWrapper.find(`[data-test="clearance_field"]`);
-        expect(security_clearance_select.text()).toContain("Yes")
-
-        const disability_select = formWrapper.find(`[data-test="disability_field"]`);
-        expect(disability_select.text()).toContain("No")
+        const formElements = findFormElements(formWrapper);
+       
+        expect(formElements.firstName.element.value).toBe("Gustavo")
+        expect(formElements.lastName.element.value).toBe("Markov")
+        expect(formElements.email.element.value).toBe('jmarkov@protonmail.com')
+        expect(formElements.phoneNumber.element.value).toBe('6478880000')
+        expect(formElements.yearsExp.element.value).toBe('10')
+        expect(formElements.age.element.value).toBe("51")
+        expect(formElements.highestEducation.text()).toContain("Bachelor's Degree")
+        expect(formElements.gender.text()).toContain("Male")
+        expect(formElements.ethnicity.text()).toContain("White")
+        expect(formElements.securityClearance.text()).toContain("Yes")
+        expect(formElements.disability.text()).toContain("No")
     });
 });
 
 describe("Fresh form after you fill in certain fields", () => {
     let formWrapper: VueWrapper<any>;
+
+    // Mandatory fields
+    let first_name_input: DOMWrapper<Element>;
+    let last_name_input: any;
+    let email_input: any;
+    let phone_number_input: any;
+    let years_exp_input: any;
+    let age_ticker: any;
+    let highest_education_select: any;
+
+    // Optional fields
+    let gender_select: any;
+    let ethnicity_select: any;
+    let security_clearance_select: any;
+    let disability_select: any;
+
     beforeEach(() => {
         // Render an empty form then fill in mandatory fields
         formWrapper = mount(PersonalDetailsForm, {
             propsData: EMPTY_FORM_PROPS,
         });
+
+        first_name_input = formWrapper.find(`[data-test="first_name_field"]`);
+        last_name_input = formWrapper.find(`[data-test="last_name_field"]`);
+        email_input = formWrapper.find(`[data-test="email_field"]`);
+        phone_number_input = formWrapper.find(`[data-test="phone_number_field"]`); // prettier-ignore
+        years_exp_input = formWrapper.find(`[data-test="years_experience_field"]`); // prettier-ignore
+
+        age_ticker = formWrapper.find(`[data-test="age_field"]`);
+        highest_education_select = formWrapper.find(`[data-test="highest_education_field"]`); // prettier-ignore
+
+        gender_select = formWrapper.find(`[data-test="gender_field"]`);
+        ethnicity_select = formWrapper.find(`[data-test="ethnicity_field"]`);
+        security_clearance_select = formWrapper.find(`[data-test="clearance_field"]`); // prettier-ignore
+        disability_select = formWrapper.find(`[data-test="disability_field"]`);
     });
 
-
-    it("Should have a successful submit when you fill in the mandatory fields correctly", () => {
+    it("Should have a successful submit when you fill in the mandatory fields correctly", async () => {
         // First Name, Last Name, Email, Phone Number, and Highest Education Level
+        await first_name_input.setValue("Gustavo");
+        await last_name_input.setValue("Markov");
+        await email_input.setValue("jmarkov@protonmail.com");
+        await phone_number_input.setValue("6478880000");
+        await years_exp_input.setValue("10");
+        // await highest_education_select.setValue('10')
+
+        await age_ticker.setValue(51);
+        forceLog(highest_education_select.html());
         expect(1).toBe(1);
     });
 
