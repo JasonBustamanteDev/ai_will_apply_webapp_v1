f5fb064c83c4045b665291e07168851210607f4f
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-09-13 13:32:01 -0400

    Start skills test form

---
+++ test/nuxt/skills.test.ts
@@ -0,0 +1,25 @@
+import { expect, describe, it, beforeEach, afterEach } from "vitest"; // prettier-ignore
+import userEvent from "@testing-library/user-event";
+import { render, screen, cleanup  } from "@testing-library/vue"; // prettier-ignore
+import SkillsForm from "~/ui/profiles/views/skills/skillsForm.vue";
+import { PROFILE_FORMS } from "~/shared/utils/globals";
+import { forceLog, forceLogElement, fillInputField, selectDropdownOption, assertDropdownValue, assertInputValue } from "../util"; // prettier-ignore
+
+// Prop data
+const ENCODED_PROFILE_NAME = "test";
+const FORM_NAME = PROFILE_FORMS.PERSONAL_DETAILS;
+
+const EMPTY_FORM_PROPS = {
+    formName: FORM_NAME,
+    encodedProfileName: ENCODED_PROFILE_NAME,
+    rawFormData: {
+        data: null,
+        isComplete: false,
+    },
+};
+
+const COMPLETED_FORM_PROPS = {
+    formName: FORM_NAME,
+    encodedProfileName: ENCODED_PROFILE_NAME,
+    rawFormData: []
+};
