d90904c8916d2050e0f43e0787a68738719ac92b
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-09-23 00:12:29 -0400

    create a modal that prompts users to install an ext

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
+import ExtensionNotInstalledModal from '~/ui/search/views/extensionNotInstalledModal.vue';
 definePageMeta({
     middleware: ["redirect-if-no-auth-session-client"],
 });
+
+const env_config = useRuntimeConfig();
+const supabaseProjectURL = env_config.public.SUPABASE_PROJECT_URL;
+
+const sendAuthDataToExtension = () => {
+    const isExtensionInstalled = document.documentElement.hasAttribute('aiwillapply-extension-installed');
+    if (isExtensionInstalled === false) {
+
+    }
+    // DO NOT DO THIS (SECURITY RISK)
+    // DO NOT DO THIS (SECURITY RISK)
+    // DO NOT DO THIS (SECURITY RISK)
+    // DO NOT DO THIS (SECURITY RISK)
+    // DO NOT DO THIS (SECURITY RISK)
+    // // Send a message to your extension - it should have a content script running on your own site
+    // const supabasePublic = "jgvuigssexmkwtoqfvbt";
+    // const authObject =
+    //     localStorage.getItem(`sb-${supabasePublic}-auth-token`) || "{}";
+
+    // window.postMessage(
+    //     {
+    //         type: "EXTENSION_AUTH_INIT",
+    //         payload: JSON.parse(authObject), //! security risk - any malicious extension can see this
+    //     },
+    //     "*" // The * here just means "send to same page"
+    // );
+};
 </script>
 
 <template>
@@ -12,6 +40,10 @@ definePageMeta({
                 FUTURE CONTENT: All the supported platforms and prefilled
                 filters
             </p>
+            <UButton @click="sendAuthDataToExtension"
+                >FIRE MESSAGE TO EXT</UButton
+            >
+            <ExtensionNotInstalledModal/>
         </div>
     </div>
 </template>
---
+++ ui/search/views/extensionNotInstalledModal.vue
@@ -0,0 +1,30 @@
+<script setup>
+const isModalOpen = ref(true);
+</script>
+
+<template>
+    <UModal
+        v-model:open="isModalOpen"
+        title="Install our 'AI will Apply' extension first"
+        description=""
+        :ui="{ footer: 'justify-end' }"
+    >
+        <template #body>
+            <p class="mb-1">Once you install the extension, reload this page and try again</p>
+            <a href="" class="text-blue-500">Click here to visit the Google Chrome store page</a>
+            <NuxtImg src="/images/misc/loom_extension.png" class="w-200px mt-4" />
+        </template>
+
+        <template #footer="{ close }">
+            <UButton
+                label="Exit"
+                color="neutral"
+                variant="outline"
+                class="cursor-pointer"
+                @click="close"
+            />
+        </template>
+    </UModal>
+</template>
+
+<style lang="scss"></style>
