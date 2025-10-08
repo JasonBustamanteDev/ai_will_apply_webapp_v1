ce38b9f1ff31c4c1b3f87127592fa4db3f7d3f7e
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-10-08 19:14:10 -0400

    Update linkedin filter vals

--- ui/search/constants/filterOptions/linkedinFilters.ts
+++ ui/search/constants/filterOptions/linkedinFilters.ts
@@ -1,47 +1,49 @@
+// tech_debt_need_match_11ASDF
+// We need to match values with certain sections of the chrome extension
 export const LINKEDIN_FILTER_OPTIONS = {
     DATE_POSTED: [
-        { label: "Any time", value: "anytime" },
-        { label: "Past month", value: "past_month" },
-        { label: "Past week", value: "past_week" },
-        { label: "Past 24 hours", value: "past_24_hours" },
+        { label: "Any time", value: "Any time" },
+        { label: "Past month", value: "Past month" },
+        { label: "Past week", value: "Past week" },
+        { label: "Past 24 hours", value: "Past 24 hours" },
     ],
     EXPERIENCE_LEVEL: [
-        { label: "Any", value: "any" }, // UNOFFICIAL OPTION (means do not apply filter)
-        { label: "Internship", value: "internship" },
-        { label: "Entry level", value: "entry_level" },
-        { label: "Associate", value: "associate" },
-        { label: "Mid-Senior level", value: "mid_senior_level" },
-        { label: "Director", value: "director" },
-        { label: "Executive", value: "executive" },
+        { label: "Any", value: "Any" }, // UNOFFICIAL OPTION (means do not apply filter)
+        { label: "Internship", value: "Internship" },
+        { label: "Entry level", value: "Entry level" },
+        { label: "Associate", value: "Associate" },
+        { label: "Mid-Senior level", value: "Mid-Senior level" },
+        { label: "Director", value: "Director" },
+        { label: "Executive", value: "Executive" },
     ],
     SALARY: [
-        { label: "Any", value: "any" }, // UNOFFICIAL OPTION (means do not apply filter)
-        { label: "$40,000+", value: "40000" },
-        { label: "$60,000+", value: "60000" },
-        { label: "$80,000+", value: "80000" },
-        { label: "$100,000+", value: "100000" },
-        { label: "$120,000+", value: "120000" },
-        { label: "$140,000+", value: "140000" },
-        { label: "$160,000+", value: "160000" },
-        { label: "$180,000+", value: "180000" },
-        { label: "$200,000+", value: "200000" },
+        { label: "Any", value: "Any" }, // UNOFFICIAL OPTION (means do not apply filter)
+        { label: "$40,000+", value: "$40,000+" },
+        { label: "$60,000+", value: "$60,000+" },
+        { label: "$80,000+", value: "$80,000+" },
+        { label: "$100,000+", value: "$100,000+" },
+        { label: "$120,000+", value: "$120,000+" },
+        { label: "$140,000+", value: "$140,000+" },
+        { label: "$160,000+", value: "$160,000+" },
+        { label: "$180,000+", value: "$180,000+" },
+        { label: "$200,000+", value: "$200,000+" },
     ],
     REMOTE: [
-        { label: "Any", value: "any" }, // UNOFFICIAL OPTION (means do not apply filter)
-        { label: "Remote", value: "remote" },
-        { label: "On-site", value: "on_site" },
-        { label: "Hybrid", value: "hybrid" },
+        { label: "Any", value: "Any" }, // UNOFFICIAL OPTION (means do not apply filter)
+        { label: "Remote", value: "Remote" },
+        { label: "On-site", value: "On-site" },
+        { label: "Hybrid", value: "Hybrid" },
     ],
     JOB_TYPE: [
         // Only appears in all filters panel
-        { label: "Any", value: "any" }, // UNOFFICIAL OPTION (means do not apply filter in extension)
-        { label: "Full-time", value: "full_time" },
-        { label: "Part-time", value: "part_time" },
-        { label: "Contract", value: "contract" },
-        { label: "Temporary", value: "temporary" },
-        { label: "Volunteer", value: "volunteer" },
-        { label: "Internship", value: "internship" },
-        { label: "Other", value: "other" },
+        { label: "Any", value: "Any" }, // UNOFFICIAL OPTION (means do not apply filter in extension)
+        { label: "Full-time", value: "Full-time" },
+        { label: "Part-time", value: "Part-time" },
+        { label: "Contract", value: "Contract" },
+        { label: "Temporary", value: "Temporary" },
+        { label: "Volunteer", value: "Volunteer" },
+        { label: "Internship", value: "Internship" },
+        { label: "Other", value: "Other" },
     ],
 };
 
