27a922fbb13458c667ef668604adb3cd4d8cce15
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-11-04 23:07:47 -0500

    remove dummy endpoints

--- server/api/devonly/sample.get.js
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const env_config = useRuntimeConfig(event)
-    
-    console.log("jj7", env_config.SUPABASE_DB_PASSWORD)
-    console.log("jj8", env_config.public.SUPABASE_PROJECT_URL)
-    return { detail: "success" };
-});
