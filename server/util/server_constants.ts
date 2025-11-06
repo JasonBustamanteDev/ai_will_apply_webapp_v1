877045229c4ca453f200f5a0a4d13526415d83d6
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-11-06 13:53:05 -0500

    up max profiles to 4

--- server/util/server_constants.ts
+++ server/util/server_constants.ts
@@ -1,4 +1,4 @@
-export const MAX_PROFILES = 3;
+export const MAX_PROFILES = 4;
 export const PROFILES_TABLE_NAME = "jobSearchProfiles";
 export const UNANSWERED_QUESTIONS_TABLE_NAME = "unansweredQuestions";
 
